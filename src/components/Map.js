8cd84a42499326abd89554fcb3d7f8dcaa706f96
konf1nement <clearddcreations@gmail.com>
2021-02-10 21:15:57 -0600

    cyclones added

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,19 +1,28 @@
 import { useState } from 'react'
 import GoogleMapReact from 'google-map-react'
-import LocationMarker from './LocationMarker'
+import LocationFire from './LocationFire'
 import LocationInfoBox from './LocationInfoBox'
+import LocationCyclone from './LocationCyclone'
 
 const Map = ({eventData, center, zoom}) => {
     const [locationInfo, setLocationInfo] = useState(null)
     const markers = eventData.map(ev => {
         if(ev.categories[0].id === 8){
-            return  <LocationMarker 
+            return  <LocationFire 
+                        lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]}
+                        onClick={() => setLocationInfo({id: ev.id, title: ev.title})}
+                    />
+        }
+
+        if(ev.categories[0].id === 10){
+            return  <LocationCyclone 
                         lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]}
                         onClick={() => setLocationInfo({id: ev.id, title: ev.title})}
                     />
         }
         return null
     })
+
     return (
         <div className ='map'>
             <GoogleMapReact
